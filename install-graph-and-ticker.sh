421895e6b8279cd9d270645c49c74d7c0bf044b7
lightningvapes <Sean@lightningvapes.com>
2017-03-31 11:42:14 -0400

    Add files via upload

--- install-graph-and-ticker.sh
+++ install-graph-and-ticker.sh
@@ -4,7 +4,9 @@ USER_HOME=$(eval echo ~${SUDO_USER})
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-chmod +x ${DIR}/crypto-tickerv2.py
+chmod +x ${DIR}/eth-crypto-tickerv2.py
+
+chmod +x ${DIR}/btc-crypto-tickerv2.py
 
 chmod +x ${DIR}/price-grabber.py
 
@@ -13,7 +15,8 @@ chmod +x ${DIR}/install-pip.py
 chmod +x ${DIR}/cloud-prices.sh
 
 crontab -l > mycron
-echo "*/2 * * * * ${DIR}/crypto-tickerv2.py" >> mycron
+echo "*/2 * * * * ${DIR}/crypto-tickerv2-eth.py" >> mycron
+echo "*/2 * * * * ${DIR}/crypto-tickerv2-btc.py" >> mycron
 # Local grabbing of prices. Use this instead of cloud-prices.sh if your computer is always on
 #echo "*/5 * * * * ${DIR}/price-grabber.py" >> mycron
 echo "*/5 * * * * ${DIR}/cloud-prices.sh" >> mycron
@@ -21,7 +24,9 @@ echo "*/5 * * * * /usr/bin/gnuplot ${DIR}/prices.gp > ${DIR}/download.png" >> my
 crontab mycron
 rm mycron
 
-${DIR}/./crypto-tickerv2.py
+${DIR}/./crypto-tickerv2-eth.py
+
+${DIR}/./crypto-tickerv2-btc.py
 
 ${DIR}/./cloud-prices.sh
 
