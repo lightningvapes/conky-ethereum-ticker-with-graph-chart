c1d575181fb4ffe50bc247c73fe0a5620ccaa164
lightningvapes <Sean@lightningvapes.com>
2017-04-03 11:43:47 -0400

    fixed colors for conky

--- get-balance.sh
+++ get-balance.sh
@@ -5,7 +5,7 @@ PROCESS="geth"
     RESULT=`pgrep ${PROCESS}`
 
     if [ "${RESULT:-null}" = null ]; then
-            echo "${PROCESS} not running. Open Mist or Ethereum Wallet"            
+            echo "Run Mist" > ~/conky-ethereum-ticker-with-graph-chart-master/miner_balance.txt          
     else
             cat <<EOF | geth attach | sed -n -r  's/^([0-9\.]+)$/\1/p' | cut -c1-5 > ~/conky-ethereum-ticker-with-graph-chart-master/miner_balance.txt
 web3.fromWei(eth.getBalance(eth.coinbase), "ether")
