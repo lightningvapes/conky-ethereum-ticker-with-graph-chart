a3b534ac2773549313aca0396fbf23fd481ea480
lightningvapes <Sean@lightningvapes.com>
2017-04-06 21:23:09 -0400

    Add files via upload

--- cloud-prices.sh
+++ cloud-prices.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-content=$(curl -L https://www.dropbox.com/s/2ko8o6q0ovajry3/prices.txt?dl=1)
+content=$(curl -L https://www.dropbox.com/s/ydowv9fzywtyt7z/prices.txt?dl=1)
 echo $content | tr " " "\n" > ~/conky-ethereum-ticker-with-graph-chart-master/prices.txt
 
