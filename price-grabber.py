a9aa3c05df19989c3c2ad42a6ede8a4c9367e89b
lightningvapes <Sean@lightningvapes.com>
2019-04-14 00:01:10 -0400

    Fixed Syntax
    Updated syntax for Python 2.7

--- price-grabber.py
+++ price-grabber.py
@@ -5,32 +5,35 @@ from os.path import expanduser
 
 home = expanduser('~')
 
-def parse():    
+def parse():
     ethereum = Ethereum()    
     file_write(ethereum)
 
 def Ethereum():
     info = []
-    price_url = "http://coinmarketcap-nexuist.rhcloud.com/api/eth/price"
+    price_url = "https://api.coinmarketcap.com/v1/ticker/ethereum/"
     r = requests.get(price_url)
     jsn_dict = r.json()
-    price = jsn_dict['usd']
-    volume_url = "http://coinmarketcap-nexuist.rhcloud.com/api/eth/volume"
+    price = jsn_dict[0]['price_usd']
+    volume_url = "https://api.coinmarketcap.com/v1/ticker/ethereum/"
     t = requests.get(volume_url)
     jsn_dict2 = t.json()
-    volume = jsn_dict2['usd']
-    change_url = "http://coinmarketcap-nexuist.rhcloud.com/api/eth/change"
+    volume = jsn_dict2[0]['24h_volume_usd']
+    change_url = "https://api.coinmarketcap.com/v1/ticker/ethereum/"
     e = requests.get(change_url)
-    change = e.json()
+    jsn_dict3 = e.json()
+    change = jsn_dict3[0]['percent_change_1h']
     info.extend((price, volume, change))
     return info
 
-def file_write(ethereum):    
+def file_write(ethereum):
     ethprice = ethereum[0]
     ethvolume = ethereum[1]
-    ethchange = ethereum[2]   
+    ethchange = ethereum[2]
     file = open("%s/conky-ethereum-ticker-with-graph-chart-master/prices.txt" % home, 'a')
-    file.write("%.2f \n" % (ethprice))
+    file.write('\n')
+    file.write((ethprice[:ethprice.find('.')+3]))
     file.close
 
 parse()
+
