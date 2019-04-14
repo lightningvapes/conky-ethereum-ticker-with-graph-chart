85901184bb2716b379cab2f47705243caf53004a
lightningvapes <Sean@lightningvapes.com>
2019-04-14 00:05:34 -0400

    Delete cloud-prices.sh
    Obselete.

--- cloud-prices.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-content=$(curl -L https://www.dropbox.com/s/ydowv9fzywtyt7z/prices.txt?dl=1)
-echo $content | tr " " "\n" > ~/conky-ethereum-ticker-with-graph-chart-master/prices.txt
-
